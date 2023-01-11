c9af19b9ca2cbca18efb24010fc57374333ff108
f1r3_r41n <qkrghkql1@gmail.com>
2023-01-11 14:20:44 +0900

    fibonacci!!!!!

--- Python/ETC/Fibonacci.py
+++ Python/ETC/Fibonacci.py
@@ -1,4 +1,35 @@
-# # 재귀함수 - 배열 사용 O
+# 예제값: 1100
+
+# dict을 사용한 재귀함수 => 272 마이크로초
+def fibo(n, __fibo_arr={0: 0, 1: 1}):
+    if n in __fibo_arr: return __fibo_arr[n]
+
+    __fibo_arr[n] = fibo(n-1)+fibo(n-2)
+    return __fibo_arr[n]
+
+num = int(input())
+print(fibo(num))
+    
+# # 재귀적 동적 계획법 => 374 마이크로초
+# def fibo(n):
+#     fibo_arr = [0, 1]
+
+#     def iterator(n):
+#         if n<2: return n
+#         try:
+#             if fibo_arr[n]: return fibo_arr[n]
+#         except IndexError:
+#             pass
+
+#         fibo_arr.append(iterator(n-1)+iterator(n-2))
+#         return fibo_arr[n]
+    
+#     return iterator(n)
+
+# num = int(input())
+# print(fibo(num))
+
+# # 재귀함수 - 배열 사용 O => 1.11 밀리초
 # fibo_arr = [0, 1]
 
 # def fibo(n):
@@ -12,14 +43,14 @@
 # print(fibo(num))
 # print(fibo_arr)
 
-# # 재귀함수 - 배열 사용 X
+# # 재귀함수 - 배열 사용 X => 요청시간 초과
 # def fibo(n):
-#     return n if n == 0 or 1 else fibo(n-1) + fibo(n-2)
+#     return n if n<=1 else fibo(n-1) + fibo(n-2)
 
 # num = int(input())
 # print(fibo(num))
 
-# # for문
+# # for문 => 137 마이크로초
 # fibo = [0, 1]
 
 # n = int(input())
@@ -28,3 +59,9 @@
 #     fibo[i%2] = fibo[(n-1)%2] + fibo[(n-2)%2]
 
 # print(fibo[n%2])
+
+# 람다 표현식 => 요청시간 초과
+# fibo = lambda n: n if n<=1 else fibo(n-1) + fibo(n-2)
+
+# num = int(input())
+# print(fibo(num))
