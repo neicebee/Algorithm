c6c973ee6a8f1f3737b5962f338738d30811d761
f1r3_r41n <qkrghkql1@gmail.com>
2023-02-04 12:13:50 +0900

    match 표현식

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -1,6 +1,33 @@
-fn main() {
-    let num = Some(5);
-    let msg = Some("Hello, world!");
+#[derive(Debug)]
+enum UsState {
+    Alabama, Alaska, Arizona,
+    Arkansas, California, Colorado,
+    Florida, Kentucky, Ohio,
+}
 
-    println!("{:?}\n{:?}", num, msg);
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}.", state);
+            25
+        },
+    }
+}
+
+fn main() {
+    let coin = Coin::Quarter(UsState::Ohio);
+    println!("{}", value_in_cents(coin));
 }
