68528b941d9a0d59cbf48b17126c3f2099b14aa4
f1r3_r41n <qkrghkql1@gmail.com>
2023-02-16 16:55:43 +0900

    Vector & String & str

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -1,6 +1,17 @@
-use std::collections::HashMap;
+#[derive(Debug)]
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
 
 fn main() {
-    let mut map = HashMap::new();
-    map.insert(1, 2);
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Float(3.14),
+        SpreadsheetCell::Text(String::from("가나다라")),
+    ];
+    
+    println!("{:#?}", row);
 }
