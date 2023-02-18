3d4f7f7756a9e3b7ed4fb49442f9cba6fa6644da
f1r3_r41n <qkrghkql1@gmail.com>
2023-02-18 14:00:04 +0900

    Rust String & &str

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -1,17 +1,4 @@
-#[derive(Debug)]
-
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
-
 fn main() {
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Float(3.14),
-        SpreadsheetCell::Text(String::from("가나다라")),
-    ];
-    
-    println!("{:#?}", row);
+    let len = String::from("안녕하세요");
+    println!("{len}");
 }
