10feaf71dc7a70ab6eb142064e30e8b69af430e0
f1r3_r41n <qkrghkql1@gmail.com>
2023-04-06 23:36:12 +0900

    stdin()

--- Rust/algorithm_of_rust/src/main.rs
+++ Rust/algorithm_of_rust/src/main.rs
@@ -1,12 +1,20 @@
-use std::io;
+use std::io::{self, Read};
 
-fn input() -> String {
-    let mut buf = String::new();
-    io::stdin().read_line(&mut buf).unwrap();
-    buf.pop();
-    buf
-}
+// fn result() -> Vec<i32> {
+//     v
+// }
 
 fn main() {
-    let n: i32 = input().parse().unwrap();
+    let mut buf = [0; 2];
+    io::stdin().read(&mut buf).unwrap();
+    let t: i32 = (buf[0]-b'0').into();
+    let mut buf2 = String::new();
+    io::stdin().read_line(&mut buf2).unwrap();
+    buf2.pop();
+    let v: Vec<i32> = buf2.split(' ')
+        .map(
+            |x| x.parse().unwrap()
+        ).collect();
+
+    println!("{t} : {:?}", v);
 }
