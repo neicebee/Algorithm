69fb61ff1b3fbaafe0051d1f4cf0017a8903b62c
f1r3_r41n <qkrghkql1@gmail.com>
2023-07-27 18:44:41 +0900

    str!!

--- Rust/algorithm_of_rust/src/main.rs
+++ Rust/algorithm_of_rust/src/main.rs
@@ -1,24 +1,47 @@
-// use std::io::{self, Read};
+// use std::io::Read;
 
 // fn main() {
+//     let mut l = [-1_i32; 26];
 //     let mut f = std::fs::File::open("src/data.txt").unwrap();
 //     let mut buf = String::new();
 //     f.read_to_string(&mut buf).unwrap();
-//     for l in buf.lines().skip(1) {
-//         let s = l.chars().map(|x| x.to_digit(10).unwrap())
-//             .sum::<u32>();
-//         println!("{s}");
+//     let b: Vec<_> = buf.as_bytes().iter().map(|x| x-97).collect();
+//     for (i, v) in b.iter().enumerate() {
+//         if l[*v as usize] == -1 {
+//             l[*v as usize] = i as i32;
+//         }
 //     }
+//     println!("{:?}", l);
 // }
 
-use std::io::{self, Read};
+// fn main() {
+//     let mut l = [-1_i32; 26];
+//     let mut f = std::fs::File::open("src/data.txt").unwrap();
+//     let mut buf = String::new();
+//     f.read_to_string(&mut buf).unwrap();
+//     let b: Vec<_> = buf.as_bytes().iter().map(|x| x-97).collect();
+//     b.iter().enumerate().for_each(
+//         |(i, v)| {
+//             if l[*v as usize] == -1 {
+//                 l[*v as usize] = i as i32;
+//             }
+//         }
+//     );
+//     println!("{:?}", l);
+// }
+
+use std::io::{self, *};
 
 fn main() {
+    let mut l = [-1_i32; 26];
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf).unwrap();
-    for l in buf.lines().skip(1) {
-        let s = l.chars().map(|x| x.to_digit(10).unwrap())
-            .sum::<u32>();
-        println!("{s}");
-    }
+    let b: Vec<_> = buf.as_bytes().iter().map(|x| x-97).collect();
+    b.iter().enumerate().for_each(
+        |(i, v)| {
+            if l[*v as usize] == -1 {
+                l[*v as usize] = i as i32;
+            }
+        }
+    );
 }
