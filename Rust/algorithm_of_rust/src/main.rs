9f1fe9c013b8c10cc7ac48d527727b0273dd7b92
f1r3_r41n <qkrghkql1@gmail.com>
2023-08-02 01:15:17 +0900

    stars@.@

--- Rust/algorithm_of_rust/src/main.rs
+++ Rust/algorithm_of_rust/src/main.rs
@@ -1,8 +1,38 @@
+// use std::io::Read;
+
+// fn main() {
+//     const PIECE: [i32; 6] = [1, 1, 2, 2, 2, 8];
+// 	let mut f = std::fs::File::open("src/data.txt").unwrap();
+//     let mut buf = String::new();
+//     f.read_to_string(&mut buf).unwrap();
+//     for (i, v) in 
+//         buf.split_whitespace().map(|x| x.parse::<i32>().unwrap()).enumerate() {
+//             print!("{} ", PIECE[i]-v);
+//     }
+// }
+
+use std::io;
+
 fn main() {
-    print!("         ,r'\"7
-r`-_   ,'  ,/
- \\. \". L_r'
-   `~\\/
-      |
-      |");
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let n = buf.trim().parse::<usize>().unwrap();
+    let mut t = 1;
+    for i in 1..=n {
+        if i==1 {
+            println!("{:>x$}", "*".repeat(t), x=n+(i-1));
+        } else {
+            println!("{:>x$}", "*".repeat(t+i), x=n+(i-1));
+            t = i;
+        }
+    }
+    t = n-1;
+    for i in (0..n-1).rev() {
+        if i==0 {
+            println!("{:>x$}", "*".repeat(t), x=n+i);
+        } else {
+            println!("{:>x$}", "*".repeat(t+i), x=n+i);
+            t = i;
+        }
+    }
 }
--- Rust/markdown/Deepening_1.md
+++ Rust/markdown/Deepening_1.md
@@ -19,8 +19,34 @@ r`-_   ,'  ,/
 
 <br>
 
-## King, Queen, 
+## King, Queen, Rook, Bishop, Knight, Pawn
 
-[Question_Link - 25083](https://www.acmicpc.net/problem/25083)
+[Question_Link - 3003](https://www.acmicpc.net/problem/3003)
 
 ### Basic Code
+
+```rust
+use std::io;
+
+fn main() {
+    const P: [i32; 6] = [1, 1, 2, 2, 2, 8];
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    for (i, v) in
+        buf.split_whitespace().map(|x| x.parse::<i32>().unwrap()).enumerate() {
+            print!("{} ", P[i]-v);
+        }
+}
+```
+
+<br>
+
+## Draw Stars - 7
+
+[Question_Link - 2444](https://www.acmicpc.net/problem/2444)
+
+### Basic Code
+
+```rust
+
+```
