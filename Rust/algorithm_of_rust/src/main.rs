25d5dc3efa0e14306ac79e5795f23ed1e5fccdc6
f1r3_r41n <qkrghkql1@gmail.com>
2023-08-04 13:07:48 +0900

    matrix!

--- Rust/algorithm_of_rust/src/main.rs
+++ Rust/algorithm_of_rust/src/main.rs
@@ -1,90 +1,19 @@
-// use std::io::Read;
-
-// fn make_array(m: &str) -> (Vec<f64>, Vec<f64>) {
-//     let mut g: Vec<f64> = Vec::new();
-//     let mut r: Vec<f64> = Vec::new();
-//     for l in m.lines() {
-//         let mut t = l.split(' ');
-//         if t.clone().nth(2).unwrap() == "P" {
-//             continue;
-//         } else {
-//             g.push(t.nth(1).unwrap().parse::<f64>().unwrap());
-//             r.push(
-//                 match t.nth(0).unwrap() {
-//                     "A+" => 4.5,
-//                     "A0" => 4.0,
-//                     "B+" => 3.5,
-//                     "B0" => 3.0,
-//                     "C+" => 2.5,
-//                     "C0" => 2.0,
-//                     "D+" => 1.5,
-//                     "D0" => 1.0,
-//                     "F" => 0.0,
-//                     _ => -1.0,
-//                 }
-//             );
-//         }
-//     }
-//     (g, r)
-// }
-
-// fn main() {
-//     let mut f = std::fs::File::open("src/data.txt").unwrap();
-//     let mut buf = String::new();
-//     f.read_to_string(&mut buf).unwrap();
-//     let mut a = 0.0;
-//     let (g, r) = make_array(buf.trim()); 
-//     if g.is_empty() && r.is_empty() {
-//         println!("{:.6}", a);
-//     } else {
-//         for i in 0..g.len() {
-//             a+=g[i]*r[i];
-//         }
-//         println!("{:.6}", a/g.iter().sum::<f64>());
-//     }
-// }
-
-use std::io::{self, *};
-
-fn make_array(m: &str) -> (Vec<f64>, Vec<f64>) {
-    let mut g: Vec<f64> = Vec::new();
-    let mut r: Vec<f64> = Vec::new();
-    for l in m.lines() {
-        let mut t = l.split(' ');
-        if t.clone().nth(2).unwrap() == "P" {
-            continue;
-        } else {
-            g.push(t.nth(1).unwrap().parse::<f64>().unwrap());
-            r.push(
-                match t.nth(0).unwrap() {
-                    "A+" => 4.5,
-                    "A0" => 4.0,
-                    "B+" => 3.5,
-                    "B0" => 3.0,
-                    "C+" => 2.5,
-                    "C0" => 2.0,
-                    "D+" => 1.5,
-                    "D0" => 1.0,
-                    "F" => 0.0,
-                    _ => -1.0,
-                }
-            );
-        }
-    }
-    (g, r)
-}
+use std::io::Read;
 
 fn main() {
+    let mut f = std::fs::File::open("src/data.txt").unwrap();
     let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf).unwrap();
-    let mut a = 0.0;
-    let (g, r) = make_array(buf.trim());
-    if g.is_empty() {
-        println!("{:.6}", a);
-    } else {
-        for i in 0..g.len() {
-            a+=g[i]*r[i];
+    f.read_to_string(&mut buf).unwrap();
+    let mut v: Vec<Vec<u32>> = Vec::new();
+    for l in buf.lines().skip(1) {
+        v.push(l.split(" ")
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>());
+    }
+    for i in 0..v.len()/2 {
+        for j in 0..v[i].len() {
+            print!("{} ", v[i][j]+v[i+v.len()/2][j]);
         }
-        println!("{:.6}", a/g.iter().sum::<f64>());
+        println!("");
     }
 }
--- Rust/markdown/Two_Dimensional_Array.md
+++ Rust/markdown/Two_Dimensional_Array.md
@@ -7,5 +7,76 @@
 ### Basic Code
 
 ```rust
+use std::io::{self, *};
 
+fn make_matrix(n: u32, m: &str) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
+    let mut v: Vec<Vec<Vec<u32>>> = Vec::new();
+    let mut iter = m.lines().skip(1);
+    for _ in 0..iter.clone().count() as u32/n {
+        let mut t: Vec<Vec<u32>> = Vec::new();
+        (0..n).for_each(|_| {
+            t.push(iter.next().unwrap().split_whitespace()
+                .map(|y| y.parse::<u32>().unwrap()).collect::<Vec<u32>>());
+        });
+        v.push(t);
+    }
+    (v[0].to_owned(), v[1].to_owned())
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let n = buf.lines().next().unwrap()
+        .split_whitespace().next().unwrap().parse::<u32>().unwrap();
+    if n == 0 {
+        println!("0");
+        return;
+    }
+    let (a, b) = make_matrix(n, buf.trim());
+    let mut r: Vec<Vec<u32>> = Vec::new();
+    for i in 0..n {
+        let mut t: Vec<u32> = Vec::new();
+        for j in 0..a[i as usize].clone().len() {
+            t.push(a[i as usize][j]+b[i as usize][j]);
+        }
+        r.push(t);
+    }
+    for i in r.iter() {
+        i.iter().for_each(|x| print!("{} ", x));
+        println!("");
+    }
+}
 ```
+
+<br>
+
+### Improvement Code
+
+```rust
+use std::io::{self, *};
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut v: Vec<Vec<u32>> = Vec::new();
+    for l in buf.lines().skip(1) {
+        v.push(l.split(" ")
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect::<Vec<u32>>());
+    }
+    for i in 0..v.len()/2 {
+        for j in 0..v[i].len() {
+            print!("{} ", v[i][j]+v[i+v.len()/2][j]);
+        }
+        println!("");
+    }
+}
+```
+
+<br>
+
+## Matrix Sum
+
+[Question_Link - 2738](https://www.acmicpc.net/problem/2738)
+
+### Basic Code
