83a4abb7865022e52db4a01cb46f788ebc2a176d
f1r3_r41n <qkrghkql1@gmail.com>
2023-09-11 11:46:37 +0900

    radix

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -1,23 +1,30 @@
-use std::fmt::Display;
+// use std::fmt::Display;
 
-fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str 
-    where T: Display
-{
-    println!("Attention! {}", ann);
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
+// fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str 
+//     where T: Display
+// {
+//     println!("Attention! {}", ann);
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let msg1 = String::from("abcd");
+//     let msg2 = String::from("xyz");
+
+//     let result = longest_with_an_announcement(
+//         msg1.as_str(), msg2.as_str(), "Everyone!!"
+//     );
+
+//     println!("result: {}", result);
+// }
 
 fn main() {
-    let msg1 = String::from("abcd");
-    let msg2 = String::from("xyz");
-
-    let result = longest_with_an_announcement(
-        msg1.as_str(), msg2.as_str(), "Everyone!!"
-    );
-
-    println!("result: {}", result);
+    let msg = "EOF";
+    for b in msg.bytes() {
+        println!("{b:x}");
+    }
 }
--- Rust/toy/src/main.rs
+++ Rust/toy/src/main.rs
@@ -7,50 +7,94 @@ C'...' 형태 : ASCII 코드로 이루어진 16진수
 use std::{fs, io::Read, error::Error, process};
 
 fn read_file() -> Result<String, Box<dyn Error>> {
-    let mut f = fs::File::open("sample.s")?;
+    let mut f = fs::File::open("numb.s")?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
     Ok(contents)
 }
 
-fn split_contents<'a>(contents: &'a str) -> (Vec<&'a str>, Vec<&'a str>, Vec<&'a str>) {
-    let mut symbol = vec![];
-    let mut command = vec![];
-    let mut operand = vec![];
-    for line in contents.lines() {
-        let words = line.split("\t").collect::<Vec<&str>>();
-        if words.len() == 4 {
-            symbol.push(words[1]);
-            command.push(words[2]);
-            operand.push(words[3]);
-        }
-    }
-    (symbol, command, operand)
-}
-
-fn print(args: Vec<&str>) {
-    for i in 0..args.len() {
-        if i%10==9 {
-            println!("{:10}", args[i]);
+fn split_contents(contents: String) -> (Vec<String>, Vec<String>, Vec<u64>) {
+    let mut hex = vec![];
+    let mut ascii_hex = vec![];
+    let mut dec = vec![];
+    for i in contents.split_ascii_whitespace() {
+        if i.contains('X') {
+            let t = i.replace('\'', "").replace('X', "");
+            println!("{}", u64::from_str_radix(&t[..], 16).unwrap());
+            hex.push(t);
+        } else if i.contains('C') {
+            let mut h = String::new();
+            let t = i.replace('\'', "").replace('C', "");
+            for b in t.bytes() {
+                h.push_str(&format!("{b:x}"));
+            }
+            ascii_hex.push(h);
         } else {
-            print!("{:10} ", args[i]);
+            dec.push(i.parse::<u64>().unwrap());
         }
     }
+    (hex, ascii_hex, dec)
 }
 
 fn main() {
     let contents = read_file().unwrap_or_else(
         |err| {
-            eprintln!("Error!\n{}", err);
+            eprintln!("Error!\n{err}");
             process::exit(1);
         }
     );
-    let (symbol, command, operand) = split_contents(&contents);
-    println!("Symbol:");
-    print(symbol);
-    println!("\nCommand:");
-    print(command);
-    println!("\nOperand:");
-    print(operand);
+    let (hex, ascii_hex, dec) = split_contents(contents);
+    println!("{:?}\n{:?}\n{:?}", hex, ascii_hex, dec);
 }
+
+// use std::{fs, io::Read, error::Error, process};
+
+// fn read_file() -> Result<String, Box<dyn Error>> {
+//     let mut f = fs::File::open("sample.s")?;
+//     let mut contents = String::new();
+//     f.read_to_string(&mut contents)?;
+
+//     Ok(contents)
+// }
+
+// fn split_contents<'a>(contents: &'a str) -> (Vec<&'a str>, Vec<&'a str>, Vec<&'a str>) {
+//     let mut symbol = vec![];
+//     let mut command = vec![];
+//     let mut operand = vec![];
+//     for line in contents.lines() {
+//         let words = line.split("\t").collect::<Vec<&str>>();
+//         if words.len() == 4 {
+//             symbol.push(words[1]);
+//             command.push(words[2]);
+//             operand.push(words[3]);
+//         }
+//     }
+//     (symbol, command, operand)
+// }
+
+// fn print(args: Vec<&str>) {
+//     for i in 0..args.len() {
+//         if i%10==9 {
+//             println!("{:10}", args[i]);
+//         } else {
+//             print!("{:10} ", args[i]);
+//         }
+//     }
+// }
+
+// fn main() {
+//     let contents = read_file().unwrap_or_else(
+//         |err| {
+//             eprintln!("Error!\n{}", err);
+//             process::exit(1);
+//         }
+//     );
+//     let (symbol, command, operand) = split_contents(&contents);
+//     println!("Symbol:");
+//     print(symbol);
+//     println!("\nCommand:");
+//     print(command);
+//     println!("\nOperand:");
+//     print(operand);
+// }
