1b05c2a84ac16640fedaa3aefafd648f7b417c84
f1r3_r41n <qkrghkql1@gmail.com>
2023-11-11 14:53:13 +0900

    mini_assembler!!

--- Rust/mini_assembler/src/main.rs
+++ Rust/mini_assembler/src/main.rs
@@ -1,3 +1,32 @@
+use std::{io::Read, fs, error::Error, process};
+
+fn read_file(file_name: &str) -> Result<String, Box<dyn Error>>{
+    let mut f = fs::File::open(file_name)?;
+    let mut buf = String::new();
+    f.read_to_string(&mut buf)?;
+    Ok(buf)
+}
+
+fn has_more_commands(contents: String) {
+    for l in contents.lines() {
+        if let Some(i) = l.find("//") {
+            command_type(&l[..i].trim());
+        } else {
+            command_type(l.trim());
+        }
+    }
+}
+
+fn command_type(command: &str) {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = read_file("prog.asm").unwrap_or_else(
+        |err| {
+            eprintln!("Error!\n{err}");
+            process::exit(1);
+        }
+    );
+    has_more_commands(contents);
 }
---
+++ Rust/mini_assembler/src/parser.rs
@@ -0,0 +1,33 @@
+pub struct Parser {
+    cur_index: i32,
+    num_of_line: i32,
+    write_file_name: String,
+}
+
+pub fn has_more_commands() {
+
+}
+
+pub fn advance() {
+
+}
+
+pub fn commandType() {
+
+}
+
+pub fn symbol() {
+
+}
+
+pub fn comp() {
+
+}
+
+pub fn dest() {
+
+}
+
+pub fn jump() {
+
+}
