bd41e17c99f0178d365aec4b9eda6be3114dc2cb
f1r3_r41n <qkrghkql1@gmail.com>
2023-11-17 15:40:00 +0900

    iterator

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -63,3 +63,13 @@ fn main() {
         simulated_random_number
     );
 }
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+    let mut v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
