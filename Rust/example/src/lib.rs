6ba67460d9102f520eac03bd383840819f6bd890
f1r3_r41n <qkrghkql1@gmail.com>
2023-11-23 15:35:21 +0900

    rust study

--- Rust/example/src/lib.rs
+++ Rust/example/src/lib.rs
@@ -1,22 +1,14 @@
-use std::error::Error;
-use std::io::Read;
-use std::env;
+/// 주어진 숫자에 1을 더한다.
+/// 
+/// # Examples
+/// 
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
 
-pub struct Config {
-    pub query: String,
-    pub filename: String,
-    pub case_sensitive: bool,
-}
-
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("필요한 인수가 지정되지 않았습니다.");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            filename: args[2].clone(),
-            case_sensitive: env::var("CASE_INSENSITIVE").is_err()
-        })
-    }
+pub fn add_one(x: i32) -> i32 {
+    x+1
 }
