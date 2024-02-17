630e1e1745be81d1d03d342c93368216210d9cbf
f1r3_r41n <qkrghkql1@gmail.com>
2024-02-17 14:42:20 +0900

    Box<T>

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -1,13 +1,24 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+use std::ops::Deref;
+
+struct MyBox<T> (T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-use List::{Cons, Nil};
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
 
 fn main() {
-    let list = Cons(1,
-        Box::new(Cons(2, 
-            Box::new(Cons(3,
-                Box::new(Nil))))));
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
