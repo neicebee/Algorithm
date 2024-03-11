c2b253c189e9de788d052b2197312a82559669f6
f1r3_r41n <qkrghkql1@gmail.com>
2024-03-11 15:28:52 +0900

    refcell<T>

--- Rust/example/src/main.rs
+++ Rust/example/src/main.rs
@@ -8,6 +8,12 @@ use std::rc::Rc;
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("a를 생성한 후의 카운터 = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    println!("b를 생성한 후의 카운터 = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("c를 생성한 후의 카운터 = {}", Rc::strong_count(&a));
+    }
+    println!("c가 범위를 벗어난 후의 카운터 = {}", Rc::strong_count(&a));
 }
